c3891ccbb007e3756033e1e1dc4db6e8f722df91
Abdulqawiyyi abiodun <86069758+horlash-Dev@users.noreply.github.com>
2026-02-12 13:06:40 +0100

    Update dashboard.blade.php test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,9 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
+                    
                 </div>
+                                    {{ __("You're logged out") }}
             </div>
         </div>
     </div>
